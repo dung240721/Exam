763b1244b3f892cc1e2fb02354f4953d5aade487
dung240721 <93905176+dung240721@users.noreply.github.com>
2023-08-02 16:01:42 +0700

    123

---
+++ emp_manager.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th8 02, 2023 lúc 09:42 AM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `emp_manager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `wage` double(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `employees`
+--
+
+INSERT INTO `employees` (`id`, `name`, `wage`) VALUES
+(1, 'Nguyen Nhat Anh', 5000.00),
+(2, 'Nguyen Dinh Hai', 5500.00),
+(3, 'Nguyen Ngoc Son', 6500.00),
+(4, 'Tran Manh Cuong', 6000.00);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- user_account.sql
+++ user_account.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 17, 2023 lúc 11:14 AM
+-- Thời gian đã tạo: Th8 02, 2023 lúc 09:42 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.0.28
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `authorities` (
   `username` varchar(100) NOT NULL,
-  `authority` varchar(50) NOT NULL DEFAULT 'ROLE_USER'
+  `authority` varchar(68) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -37,8 +37,9 @@ CREATE TABLE `authorities` (
 --
 
 INSERT INTO `authorities` (`username`, `authority`) VALUES
-('admin1', 'ROLE_ADMIN'),
-('user1', 'ROLE_USER');
+('admin', 'ROLE_ADMIN'),
+('manager', 'ROLE_MANAGER'),
+('user', 'ROLE_USER');
 
 -- --------------------------------------------------------
 
@@ -57,8 +58,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
-('admin1', '{bcrypt}$2a$10$PvD1bL9MYfzqoJMWmHRjuunzQTGM3GtTTIzRzH5mTOCeAEK68QRbO', 1),
-('user1', '{bcrypt}$2a$10$qC/SmGTpEFnRj8VH8zY0P.Qv1uEbtTrrf2OykHNFBiqSSxY0YnHOa', 1);
+('admin', '{bcrypt}$2a$10$PvD1bL9MYfzqoJMWmHRjuunzQTGM3GtTTIzRzH5mTOCeAEK68QRbO', 1),
+('manager', '{bcrypt}$2a$10$0Iv6ngTRcV6jvScKVTStquCgvifH3OhCA44/9XNynEe8GNtkV1MCy', 1),
+('user', '{bcrypt}$2a$10$qC/SmGTpEFnRj8VH8zY0P.Qv1uEbtTrrf2OykHNFBiqSSxY0YnHOa', 1);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -68,7 +70,7 @@ INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
 -- Chỉ mục cho bảng `authorities`
 --
 ALTER TABLE `authorities`
-  ADD KEY `foreign` (`username`);
+  ADD KEY `username` (`username`);
 
 --
 -- Chỉ mục cho bảng `users`
@@ -84,7 +86,7 @@ ALTER TABLE `users`
 -- Các ràng buộc cho bảng `authorities`
 --
 ALTER TABLE `authorities`
-  ADD CONSTRAINT `foreign` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
+  ADD CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
