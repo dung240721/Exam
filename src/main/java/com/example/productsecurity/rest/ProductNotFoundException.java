df7453513c2c5288a2f92b86a94f2c9850c88a39
dung240721 <93905176+dung240721@users.noreply.github.com>
2023-08-28 16:38:20 +0700

    1
    123

--- src/main/java/com/example/productsecurity/rest/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.productsecurity.rest;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(Integer id) {
-        super("Không tìm thấy sản phẩm có id: " + id);
-    }
-}
